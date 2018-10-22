5f4d6fedfe35909a0d0e1f9844dbb4cdbffe1f19
Mostafa Hirsi <Mostafa.Hirsi@vigosoftware.com>
2018-10-22 12:05:46 +0100

    villain api add

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_villains/villains/villains.dart';
 import 'package:mymovieapp/views/search.dart';
 
 void main() => runApp(new MyApp());
